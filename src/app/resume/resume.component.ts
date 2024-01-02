043f367ad8773aa51f0517f0cbccce2c369805d9
vocaltech <it@vocaltech.fr>
2024-01-02 19:16:32 +0100

    Initial commit

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,15 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
+import { NgIf } from '@angular/common'
 import { Title } from '@angular/platform-browser';
+import { AccordionModule } from 'ngx-bootstrap/accordion'
 
 @Component({
   selector: 'app-resume',
   standalone: true,
-  imports: [],
+  imports: [
+    AccordionModule,
+    NgIf
+  ],
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.css'
 })
 export class ResumeComponent {
-  constructor(private titleService: Title) {
+  isWorkExperienceAccordionOpened: boolean = false
+
+  constructor(private titleService: Title, private renderer: Renderer2) {
     this.titleService.setTitle("Vocaltech - Resume")
+  }
+
+  downloadPdfResume = () => {
+    const downloadLink: HTMLElement = this.renderer.createElement('a')
+    downloadLink.setAttribute('target', '_blank')
+    downloadLink.setAttribute('href', '/assets/sample_resume.pdf')
+    downloadLink.setAttribute('download', 'resume-cv.pdf')
+    downloadLink.click()
+    downloadLink.remove()
   }
 }
