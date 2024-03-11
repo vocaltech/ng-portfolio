59a73194ce2c162dfe364744e5b52f2ec28f3261
vocaltech <it@vocaltech.fr>
2024-03-11 23:35:33 +0100

    Add SafeHtmlPipe import

--- src/app/project-modal/project-modal.component.ts
+++ src/app/project-modal/project-modal.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap/modal';
 import { CarouselModule } from 'ngx-bootstrap/carousel'
 import { Project } from '../_models/Project';
-import { NgFor } from '@angular/common'
+import { NgFor } from '@angular/common';
+import { SafeHtmlPipe } from '../_pipes/safehtml.pipe'
 
 @Component({
   selector: 'app-project-modal',
   standalone: true,
   imports: [
     CarouselModule,
-    NgFor
+    NgFor,
+    SafeHtmlPipe
   ],
   templateUrl: './project-modal.component.html',
   styleUrl: './project-modal.component.css'
